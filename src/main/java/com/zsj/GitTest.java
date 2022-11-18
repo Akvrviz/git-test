9508af246feb63299b17031ac6e83b018d0d25a9
Akvrviz <ZSJ15362531401@outlook.com>
2022-11-18 21:21:20 +0800

    second commit

--- src/main/java/com/zsj/GitTest.java
+++ src/main/java/com/zsj/GitTest.java
@@ -3,5 +3,6 @@ package com.zsj;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !!! hello world!!!");
+        System.out.println("第二次修改 hello git!!!");
     }
 }
